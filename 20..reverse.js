b6d3f01794b87465302735b0fbee5a36383750bd
abdul-rahman <Abdulrahman.81869@gmail.com>
2024-11-13 23:49:29 +0530

    Array Methods

---
+++ 20..reverse.js
@@ -0,0 +1,20 @@
+/*
+ the reverse () method reverses the order of the elements in an array in place.
+
+ syntax: arrays.reverse(0)
+
+*/
+
+let letters = ["a", "b", "c", "d"];
+letters.reverse();
+console.log(letters);
+
+let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h"];
+alphabets.reverse();
+console.log(alphabets);
+
+
+
+let numbers = [1, 2, 34, 45, 66, 777];
+numbers.reverse();
+console.log(numbers);
